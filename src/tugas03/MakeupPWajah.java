5cea552d9afdf5a6c9c5d7116791208c1413d93e
galuhnurdianty <32261792+galuhnurdianty@users.noreply.github.com>
2018-04-07 20:59:49 +0800

    atribute dan method

--- src/tugas03/MakeupPWajah.java
+++ src/tugas03/MakeupPWajah.java
@@ -1,5 +1,10 @@
 package tugas03;
 
 public class MakeupPWajah extends Makeup{
+    private String namaMakeup;
+    private String Untuk;
     
+    void throttle(){
+        System.out.println("Ini Adalah Sabun Pembersih Wajah ^_^ ");
+    }
 }
