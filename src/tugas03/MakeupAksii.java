4a945d118b6bc40b7433343611b11c193e201b9a
galuhnurdianty <32261792+galuhnurdianty@users.noreply.github.com>
2018-04-07 20:06:15 +0800

    Instance, buat atribut dan panggil method

--- src/tugas03/MakeupAksii.java
+++ src/tugas03/MakeupAksii.java
@@ -1,6 +1,12 @@
 package tugas03;
 public class MakeupAksii {
     public static void main(String[] args) {
-        
+        Makeup m = new Makeup();
+       m.merk="Wardah";
+       m.jenis="Gincu";
+       m.nomor="05";
+       m.harga=50000;
+       
+       m.cetakInfo();
     }
 }
