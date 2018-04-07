97c0bf6b5228618d0d5369891cda8386af4329fd
galuhnurdianty <32261792+galuhnurdianty@users.noreply.github.com>
2018-04-07 21:00:59 +0800

    Getter and Setter

--- src/tugas03/MakeupPWajah.java
+++ src/tugas03/MakeupPWajah.java
@@ -7,4 +7,22 @@ public class MakeupPWajah extends Makeup{
     void throttle(){
         System.out.println("Ini Adalah Sabun Pembersih Wajah ^_^ ");
     }
+
+    public String getNamaMakeup() {
+        return namaMakeup;
+    }
+
+    public void setNamaMakeup(String namaMakeup) {
+        this.namaMakeup = namaMakeup;
+    }
+
+    public String getUntuk() {
+        return Untuk;
+    }
+
+    public void setUntuk(String Untuk) {
+        this.Untuk = Untuk;
+    }
+    
+    
 }
