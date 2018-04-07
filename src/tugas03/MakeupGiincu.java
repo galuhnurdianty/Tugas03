046d71ad7327a814fbcdc3ccfe1462a15888e7d3
galuhnurdianty <32261792+galuhnurdianty@users.noreply.github.com>
2018-04-07 20:34:57 +0800

    atribut dan method

--- src/tugas03/MakeupGiincu.java
+++ src/tugas03/MakeupGiincu.java
@@ -1,4 +1,8 @@
 package tugas03;
 public class MakeupGiincu extends Makeup{
+     private String namaMakeup;
     
+    void throttle(){
+        System.out.println("Ini Adalah Gincu :* ");
+    }
 }
