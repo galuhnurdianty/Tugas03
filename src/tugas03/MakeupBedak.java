1f78a19e9f4cda3411cce479c4e2631ecf48558e
galuhnurdianty <32261792+galuhnurdianty@users.noreply.github.com>
2018-04-07 20:50:33 +0800

    atribute dan method

--- src/tugas03/MakeupBedak.java
+++ src/tugas03/MakeupBedak.java
@@ -1,4 +1,9 @@
 package tugas03;
 public class MakeupBedak extends Makeup{
+     private String namaMakeup;
+    private String tekstureMakeup;
     
+    void throttle(){
+        System.out.println("Ini Adalah Pupur :) ");
+    }
 }
